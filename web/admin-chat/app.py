59d8c875e9df38b3620051fb7b775347de384ae6
Jakob Löw <jakob@m4gnus.de>
2023-06-29 10:12:02 +0200

    :bug: fix minor problems with admin chat allowing to exploit the XSS vulnerability

--- web/admin-chat/app.py
+++ web/admin-chat/app.py
@@ -1,6 +1,7 @@
 import os
 import time
 import random
+import traceback
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
 from urllib.parse import urlparse, parse_qs
@@ -8,8 +9,8 @@ from urllib.parse import urlparse, parse_qs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-EASY_CHALLENGE_URL = os.getenv("EASY_CHALLENGE_URL") or "localhost:8081"
-HARD_CHALLENGE_URL = os.getenv("HARD_CHALLENGE_URL") or "localhost:8082"
+EASY_CHALLENGE_URL = os.getenv("EASY_CHALLENGE_URL")
+HARD_CHALLENGE_URL = os.getenv("HARD_CHALLENGE_URL")
 EASY_CHALLENGE_FLAG = os.getenv("EASY_CHALLENGE_FLAG")
 HARD_CHALLENGE_FLAG = os.getenv("HARD_CHALLENGE_FLAG")
 try:
@@ -31,8 +32,18 @@ MESSAGES_FAILURE = [
     "Please send a working link next time",
 ]
 
+def getChallengeUrl(prefixes, url):
+    for option in prefixes.split(','):
+        if url.startswith(option):
+            return option
+
+    return None
+
 def open_url_as_admin(url):
-    if not url.startswith(EASY_CHALLENGE_URL) and not url.startswith(HARD_CHALLENGE_URL):
+    challenge_prefix = getChallengeUrl(EASY_CHALLENGE_URL, url)
+    if not challenge_prefix:
+        challenge_prefix = getChallengeUrl(HARD_CHALLENGE_URL, url)
+    if not challenge_prefix:
         return False
 
     chrome_options = Options()
@@ -41,31 +52,29 @@ def open_url_as_admin(url):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox") # TODO?!
-    driver = webdriver.Chrome("chromedriver", chrome_options=chrome_options)
+    driver = webdriver.Chrome("chromedriver", options=chrome_options)
 
     success = False
     try:
         # https://stackoverflow.com/a/28331099
-        if url.startswith(EASY_CHALLENGE_URL):
-            driver.get(EASY_CHALLENGE_URL + "/404")
-            driver.add_cookie({'name': 'PASSWORD', 'value': EASY_CHALLENGE_FLAG, 'domain': EASY_CHALLENGE_URL})
-        elif url.startswith(HARD_CHALLENGE_URL):
-            driver.get(HARD_CHALLENGE_URL + "/404")
-            driver.add_cookie({'name': 'PASSWORD', 'value': HARD_CHALLENGE_FLAG, 'domain': HARD_CHALLENGE_URL})
+        driver.get(challenge_prefix + "/404")
+        driver.add_cookie({'name': 'PASSWORD', 'value': EASY_CHALLENGE_FLAG, 'domain': challenge_prefix.replace('https://', '')})
+        time.sleep(0.5)
 
         driver.get(url)
         time.sleep(3)
         success = True
     except:
-        pass
+        traceback.print_exc()
     finally:
         driver.quit()
 
     return success
 
 class RequestHandler(BaseHTTPRequestHandler):
-    def send(self, status, msg):
+    def send(self, status, mime, msg):
         self.send_response(status)
+        self.send_header("Content-type", mime)
         self.end_headers()
         self.wfile.write(msg.encode("utf-8"))
 
@@ -76,20 +85,20 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         if path == "/":
             with open("frontend/index.html", "r") as fd:
-                self.send(200, fd.read())
+                self.send(200, "text/html", fd.read())
         elif path in ["/styles.css", "/client.js", ""]:
             with open("frontend" + path, "r") as fd:
-                self.send(200, fd.read())
+                self.send(200, "text/css", fd.read())
         elif self.path.startswith("/send") and "url" in params:
             success = open_url_as_admin(params["url"][0])
-            self.send(200, random.choice(MESSAGES_SUCCESS if success else MESSAGES_FAILURE))
+            self.send(200, "text/plain", random.choice(MESSAGES_SUCCESS if success else MESSAGES_FAILURE))
         else:
-            self.send(404, "not found")
+            self.send(404, "text/plain", "not found")
 
 class ThreadingHttpServer(ThreadingMixIn, HTTPServer):
     pass
 
 if __name__ == "__main__":
-    print(f"Starting on port {PORT} with challenge URLs {EASY_CHALLENGE_URL} and {MEDIUM_CHALLENGE_URL}")
+    print(f"Starting on port {PORT} with challenge URLs {EASY_CHALLENGE_URL} and {HARD_CHALLENGE_URL}")
     httpd = ThreadingHttpServer(("0.0.0.0", PORT), RequestHandler)
     httpd.serve_forever()
